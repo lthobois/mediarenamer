972205297db5e13a841425bb9d02e4a0042838c7
codename.matrix <codename.matrix@b20df95d-5d36-0410-980b-a127417845c5>
2010-03-21 17:09:47 +0000

    Fixed IMDB parser for new imdb search engine

--- trunk/MediaRenamer/Movies/OnlineParserIMDB.cs
+++ trunk/MediaRenamer/Movies/OnlineParserIMDB.cs
@@ -16,30 +16,14 @@ namespace MediaRenamer.Movies {
 
             if (data == null) return;
 
-            String pat = "<A HREF=\"/title/tt([0-9]*)/\">([^<]*)</A>";
+            String pat = "<a href=\"/title/tt([0-9]*)/\">([^<]*)</a>[^<]+<span class=\"year_type\">([^<]+)</span>";
             Regex reg = new Regex(pat);
             MatchCollection mcol = null;
             mcol = reg.Matches(data);
             List<movieData> movieNames = new List<movieData>();
-            if (mcol.Count == 1) {
-                if (mcol[0].Groups[2].Captures[0].Value.IndexOf("(VG)") < 0) {
-                    movieNames.Add(parseMovie(mcol[0].Groups[2].Captures[0].Value));
-                    String[] alternates = findAlternatives(data, mcol[0].Groups[0].Captures[0].Value);
-                    foreach (String alt in alternates) {
-                        movieNames.Add(parseMovie(alt));
-                    }
-                }
-            }
-            else if (mcol.Count > 1) {
+            if (mcol.Count > 0) {
                 foreach (Match m in mcol) {
-                    if (m.Groups[2].Captures[0].Value.IndexOf("(VG)") > 0) continue;
-
-                    movieNames.Add(parseMovie(m.Groups[2].Captures[0].Value));
-
-                    String[] alternates = findAlternatives(data, m.Groups[0].Captures[0].Value);
-                    foreach (String alt in alternates) {
-                        movieNames.Add(parseMovie(alt));
-                    }
+                    movieNames.Add(parseMovie(m.Groups[2].Captures[0].Value, m.Groups[3].Captures[0].Value));
                 }
             }
 
@@ -47,70 +31,18 @@ namespace MediaRenamer.Movies {
             setMovieTitle(ref movie, selectedMovie);
         }
 
-        private movieData parseMovie(String movieName) {
+        private movieData parseMovie(String movieName, String year) {
             movieData md = new movieData();
             md.Name = movieName;
-            String pat = @"\(([0-9]{4})(|[^)]*)\)";
+            String pat = @"([0-9]{4})";
             Regex reg = new Regex(pat);
             Match m = null;
-            m = reg.Match(md.Name);
+            m = reg.Match(year);
             if (m.Success) {
-                int pos = md.Name.IndexOf(m.Groups[0].Captures[0].Value);
-                md.Name = md.Name.Substring(0, pos);
-                md.Name = md.Name.Trim();
-                pos = md.Name.LastIndexOf(",");
-                if (pos > 0) {
-                    String ending = md.Name.Substring(pos);
-                    if (movePrefix.IndexOf(ending) != -1) {
-                        md.Name = md.Name.Substring(pos + 1) + " " + md.Name.Substring(0, pos);
-                    }
-                }
-                md.Name = md.Name.Trim();
-                md.Year = Int32.Parse(m.Groups[1].Captures[0].Value);
+                md.Year = Int32.Parse(m.Groups[0].Captures[0].Value);
             }
 
             return md;
-        }
-
-        string requestImdbPost(string uri, string parameters) {
-            // parameters: name1=value1&name2=value2	
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
-            //string ProxyString = 
-            //   System.Configuration.ConfigurationManager.AppSettings
-            //   [GetConfigKey("proxy")];
-            //webRequest.Proxy = new WebProxy (ProxyString, true);
-            //Commenting out above required change to App.Config
-            webRequest.ContentType = "application/x-www-form-urlencoded";
-            webRequest.Method = "POST";
-            webRequest.UserAgent = "Mozilla/5.0";
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(parameters);
-            Stream os = null;
-            try { // send the Post
-                webRequest.ContentLength = bytes.Length;   //Count bytes to send
-                os = webRequest.GetRequestStream();
-                os.Write(bytes, 0, bytes.Length);         //Send it
-            }
-            catch (WebException ex) {
-                MessageBox.Show(ex.Message, "HttpPost: Request error",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally {
-                if (os != null) {
-                    os.Close();
-                }
-            }
-
-            try { // get the response
-                WebResponse webResponse = webRequest.GetResponse();
-                if (webResponse == null) { return null; }
-                StreamReader sr = new StreamReader(webResponse.GetResponseStream());
-                return sr.ReadToEnd().Trim();
-            }
-            catch (WebException ex) {
-                MessageBox.Show(ex.Message, "HttpPost: Response error",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            return null;
         }
 
         private String loadMovieData(ref Movie movie) {
@@ -129,12 +61,14 @@ namespace MediaRenamer.Movies {
                 String movieEnc = movieName;
                 movieEnc = movieEnc.Replace("-", "+");
                 movieEnc = movieEnc.Replace(" ", "+");
+                movieEnc = movieEnc.Replace(movie.year.ToString(), "");
 
-                String param = "tv=on&ep=off&words=" + movieEnc;
-                if (movie.year > 0) {
-                    param += "&year=" + movie.year;
-                }
-                data = requestImdbPost("http://us.imdb.com/List", param);
+                String url = "http://www.imdb.com/search/title?";
+                url += String.Format("release_date={0},", movie.year.ToString());
+                url += String.Format("&title={0}", movieEnc);
+                url += "&title_type=feature,tv_movie,tv_series,mini_series";
+                WebClient cli = new WebClient();
+                data = cli.DownloadString(url);
                 StreamWriter swr = new StreamWriter(movieCache, false);
                 swr.Write(data);
                 swr.Close();
@@ -146,22 +80,6 @@ namespace MediaRenamer.Movies {
                 sreader.Close();
             }
             return data;
-        }
-
-        private string[] findAlternatives(String data, String p) {
-            String[] result = new String[0];
-            String aka = data.Substring(data.IndexOf(p));
-            aka = aka.Substring(0, aka.IndexOf("</LI>"));
-            string split = "...aka";
-            while (aka.IndexOf(split) > 0) {
-                String akName = aka.Substring(aka.IndexOf(split) + split.Length);
-                akName = akName.Substring(0, akName.IndexOf(")") + 1);
-                aka = aka.Replace(split + akName, "");
-                akName = akName.Trim();
-                Array.Resize<String>(ref result, result.Length + 1);
-                result[result.Length - 1] = akName;
-            }
-            return result;
         }
     }
 }
